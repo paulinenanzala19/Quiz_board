d27ef25343fe00ad45575135ed380ae7ffa38c41
paulinenanzala19 <pauline.wafula@student.moringaschool.c0m>
2022-02-06 16:28:22 +0300

    style:paragraph-styling-styling the answers

--- js/script.js
+++ js/script.js
@@ -6,5 +6,7 @@ function myButton(){
     var currentAnswer=document.questions.question4.value;
     var currentAnswer=document.questions.question5.value;
     if (currentAnswer=="Boolean"){correct+=20};
+    if (currentAnswer=="one cannot write multiple codes without executing them"){correct+=20}
+    if (currentAnswer=="It is a collection of codes that can be reused")
     document.write("your score"+correct);
 }
