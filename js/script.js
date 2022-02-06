ba15170766e7a69c51b241e76e6354cbe4ee5c0b
paulinenanzala19 <pauline.wafula@student.moringaschool.c0m>
2022-02-06 18:40:18 +0300

    style:head-styling-groped the heading under form

--- js/script.js
+++ js/script.js
@@ -1,12 +1,19 @@
 function myButton(){
-    var correct=0;
-    var currentAnswer=document.questions.question1.value;
-    var currentAnswer=document.questions.question2.value;
-    var currentAnswer=document.questions.question3.value;
-    var currentAnswer=document.questions.question4.value;
-    var currentAnswer=document.questions.question5.value;
-    if (currentAnswer=="Boolean"){correct+=20};
-    if (currentAnswer=="one cannot write multiple codes without executing them"){correct+=20}
-    if (currentAnswer=="It is a collection of codes that can be reused")
-    document.write("your score"+correct);
-}
+    var score=0;
+    var currentAnswer1=document.questions.question1.value;
+    var currentAnswer2=document.questions.question2.value;
+    var currentAnswer3=document.questions.question3.value;
+    var currentAnswer4=document.questions.question4.value;
+    var currentAnswer5=document.questions.question5.value;
+    var result=document.getElementById('feedback')
+    if (currentAnswer1=="Boolean"){score+=20};
+    if (currentAnswer2=="One cannot write multiple codes without executing them"){score+=20};
+    if (currentAnswer3=="It is a collection code that can be reused"){score+=20};
+    if (currentAnswer4=="center"){score+=20};
+    if (currentAnswer5=="Allow us to run the code multiple times depending on whether a condition remains true"){score+=20};
+    questions.style.display="none"
+    result.textContent=score;
+    // document.write("your score"+correct);
+    // document.write(score);
+    
+ }
